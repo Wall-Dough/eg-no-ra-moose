90a77d7487f2068fdb38b51a831de9479995ba0e
Andrew Walden <awalden0808@gmail.com>
2017-05-15 15:10:51 -0400

    Fix tabs

--- Board.java
+++ Board.java
@@ -4,64 +4,64 @@ import java.util.list;
 public class Board {
 
 	
-	private List<Peg> pegs;
+    private List<Peg> pegs;
 	
-	public void addJump(int jumpTo, int toJump) {
-		this.pegs.get(i).addJump(this.pegs.get(jumpTo), this.pegs.get(toJump));
-		this.pegs.get(jumpTo).addJump(this.pegs.get(i), this.pegs.get(toJump));
-	}
+    public void addJump(int jumpTo, int toJump) {
+    	this.pegs.get(i).addJump(this.pegs.get(jumpTo), this.pegs.get(toJump));
+    	this.pegs.get(jumpTo).addJump(this.pegs.get(i), this.pegs.get(toJump));
+    }
+    
+    public Board duplicate() {
+    	Board newBoard = new Board();
 
-	public Board duplicate() {
-		Board newBoard = new Board();
+        for (int i = 0; i < 15; i++) {
+    	    newBoard.pegs.add(this.pegs.get(i).duplicate());
+    	}
 
-		for (int i = 0; i < 15; i++) {
-			newBoard.pegs.add(this.pegs.get(i).duplicate());
-		}
+	return newBoard;
+    }
 
-		return newBoard;
-	}
-
-	public void build() {
-		this.pegs = new List<>();
+    public void build() {
+    	this.pegs = new List<>();
 		
-		for (int i = 0; i < 15; i++) {
-			this.pegs.add(new Peg(true));
-		}
+        for (int i = 0; i < 15; i++) {
+            this.pegs.add(new Peg(true));
+        }
+    
+        this.pegs.get(0).setStatus(false);
+        
+        int numInRow = 1;
+	int rowStart = 0;
 
-		this.pegs.get(0).setStatus(false);
-
-		int numInRow = 1;
-		int rowStart = 0;
-
-		for (int i = 0; i < 6; i++) {
-			if (i >= (rowStart + numInRow)) {
-				rowStart += numInRow;
-				numInRow++;
-			}
-
-			int toJump = i + numInRow;
-			int jumpTo = toJump + numInRow + 1;
-			this.addJump(jumpTo, toJump);
-			
-			toJump += 1;
-			jumpTo += 2;
-			this.addJump(jumpTo, toJump);
-		}
-
-		numInRow = 1;
-		rowStart = 0;
-
-		for (int i = 0; i < 15; i++) {
-			if (i >= (rowStart + numInRow)) {
-				rowStart += numInRow;
-				numInRow++;
-			}
-
-			if ((i + 2) < (rowStart + numInRow)) {
-				toJump = i + 1;
-				jumpTo = i + 2;
-				this.addJump(jumpTo, toJump);
-			}
-		}
+    	for (int i = 0; i < 6; i++) {
+            if (i >= (rowStart + numInRow)) {
+                rowStart += numInRow;
+                numInRow++;
+            }
+            
+            int toJump = i + numInRow;
+            int jumpTo = toJump + numInRow + 1;
+            this.addJump(jumpTo, toJump);
+            
+            toJump += 1;
+            jumpTo += 2;
+	    this.addJump(jumpTo, toJump);
 	}
+
+	numInRow = 1;
+	rowStart = 0;
+
+	for (int i = 0; i < 15; i++) {
+	    if (i >= (rowStart + numInRow)) {
+    		rowStart += numInRow;
+	    	numInRow++;
+	    }
+
+	    if ((i + 2) < (rowStart + numInRow)) {
+		toJump = i + 1;
+		jumpTo = i + 2;
+		this.addJump(jumpTo, toJump);
+	    }
+	}
+    }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Peg peg = new Peg(true);
-	}
+
+    public static void main(String[] args) {
+    	Peg peg = new Peg(true);
+    }
 }
